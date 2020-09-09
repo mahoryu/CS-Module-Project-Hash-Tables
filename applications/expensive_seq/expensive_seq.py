a94ae61fb0f1c147c2685bf66abde3055e63ffb5
Ethan Holden <mahoryu@gmail.com>
2020-09-08 20:45:08 -0600

    Passed the test for the expensive sequence

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,9 +1,17 @@
 # Your code here
-
+lookup_table = {}
 
 def expensive_seq(x, y, z):
-    # Your code here
-
+    if x <= 0:
+        return y + z
+    if x > 0:
+        result = 0
+        for i in range(1,4):
+            if not (x-i,y+i,z*i) in lookup_table:
+                lookup_table[(x-i,y+i,z*i)] = expensive_seq(x-i,y+i,z*i)
+            result += lookup_table[(x-i,y+i,z*i)]
+        lookup_table[(x,y,z)] = result
+        return result
 
 
 if __name__ == "__main__":
