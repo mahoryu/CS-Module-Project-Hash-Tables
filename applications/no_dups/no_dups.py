3846c972142bda690003a5b7c6a9610e33295c8f
Ethan Holden <mahoryu@gmail.com>
2020-09-10 19:39:57 -0600

    Passed the test for no dups

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,7 +1,14 @@
 def no_dups(s):
-    # Your code here
-
-
+    lookup_table = {}
+    for word in s.split():
+        if word in lookup_table.keys():
+            continue
+        else:
+            lookup_table[word] = 1
+    output = ""
+    for key in lookup_table:
+        output += f"{key} "
+    return output[:-1]
 
 if __name__ == "__main__":
     print(no_dups(""))
